2017736604c1e5ad973f8b7520c4f5f75b59a940
DevProper <jacqueshugo@hotmail.fr>
2017-02-18 16:42:06 +0100

    Build board from cis file
    _build_from_cis(path, game_board)

--- Projet.py
+++ Projet.py
@@ -9,11 +9,20 @@ def _add_ship(player, position, ship_info, game_board):
     game_board[position][player].update(ship_info)
     
  
-##def _build_from_cis(path, game_board):
+def _build_from_cis(path, game_data):
+    fh = open(path, 'r')
     
-       
-          
-             
+    if not fh:
+        return 0
+        
+    lines_list = fh.readlines();
+    for line in lines_list[1:]:
+        line_elements = line.split(' ') #split the line to get each element
+        ship_name = line_elements[2].split(':')#split to get the ship name and type
+        
+        ship_info = {ship_name[0]:{'type': ship_name[1], 'orientation':line_elements[3]}} #build shit info (type, orientation)
+        
+        _add_ship(0, (int(line_elements[0]), int(line_elements[1])), ship_info, game_data) #cast str to int to get the coordonates
                 
                    
  ###TEST ZONE###                     
@@ -22,17 +31,10 @@ def _add_ship(player, position, ship_info, game_board):
                                   
 game_data ={}
 _build_board(game_data, 40)
-fh = open('C:/Users/TEST/Desktop/test.cis', 'r')
+_build_from_cis('C:/Users/Hugo/Desktop/test.cis', game_data)
 
-lines_list = fh.readlines()
-for line in lines_list[1:]: #process each line
-        line_element = line.split(' ') #split the line to get each element
-        ship_name = line_element[2].split(':')#split to get the ship name and type
-        ship_info = {ship_name[0]:{'type': ship_name[1], 'orientation':line_element[3]}} #build shit info (type, orientation)
-        _add_ship(0, (int(line_element[0]), int(line_element[1])), ship_info, game_data)
-        print game_data[(int(line_element[0]), int(line_element[1]))]
+print game_data
 
-fh.close()
 
 
 
